74bd0fba0f69cf3f0d152dd291bdad4766525090
rahebm <mehmetraheb@gmail.com>
2023-10-29 12:52:09 -0400

    ngfrm3

--- src/test/java/testCases/loginTest.java
+++ src/test/java/testCases/loginTest.java
@@ -1,6 +1,7 @@
 package testCases;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utils.CommonMethods;
 
@@ -20,4 +21,37 @@ public class loginTest extends CommonMethods {
      //verification
         Assert.assertEquals(actualMessage, "Welcome Admin");
     }
+    //    test 4 scenarios with invalid credentials
+
+    //    1.declare the data in 2D array
+    @DataProvider(name="credentials")
+    public Object [][] data(){
+
+        Object[][] loginData={
+                {"Admin","abc","Invalid credential"},
+                {"Admin","xyz","Invalid credentials"},
+                {"Admin","","Password cannot be empty"},
+                {"","hum","Username cannot be empty"}
+        };
+        return  loginData;
+    }
+
+    @Test(dataProvider="credentials")
+    public void invalidCredentials(String userName , String password , String errorMsg) {
+        sendText(login.usernameBox,userName);
+        sendText(login.passwordBox,password);
+        click(login.loginBtn);
+//        verify the error message
+        String ActualError = login.errorMessage.getText();
+
+//        assertion
+        Assert.assertEquals(ActualError,errorMsg);
+
+    }
+
+
+
+
+
+
 }
